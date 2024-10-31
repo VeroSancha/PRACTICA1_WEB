e96fe57ac997a49e27c24fb0a62ed7f03580991a
VeroSancha <versanma@gmail.com>
2024-10-31 14:59:06 +0100

    Carrousel-cards
    Al final el carrousel con flickity :/

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("Funciona por dios");
+
+// inicializar flickity 
+
+$('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+    pauseAutoPlayOnHover: false
+  });
+
+  $('.card-slider').flickity({
+    cellAlign: 'left',
+    contain: true,
+    freeScroll: true,
+    wrapAround: true,
+   
+  });
